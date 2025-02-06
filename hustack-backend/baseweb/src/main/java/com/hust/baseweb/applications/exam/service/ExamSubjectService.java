d9c5e93716b4d81098f733b4b16b289d3211e840
docongban <banto2172001@gmail.com>
2025-02-06 20:54:22 +0700

    merge code from commit 78a90bc

---
+++ hustack-backend/baseweb/src/main/java/com/hust/baseweb/applications/exam/service/ExamSubjectService.java
@@ -0,0 +1,20 @@
+package com.hust.baseweb.applications.exam.service;
+
+import com.hust.baseweb.applications.exam.entity.ExamSubjectEntity;
+import com.hust.baseweb.applications.exam.model.ResponseData;
+import com.hust.baseweb.applications.exam.model.request.ExamSubjectDeleteReq;
+import com.hust.baseweb.applications.exam.model.request.ExamSubjectFilterReq;
+import com.hust.baseweb.applications.exam.model.request.ExamSubjectSaveReq;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface ExamSubjectService {
+
+    Page<ExamSubjectEntity> filter(Pageable pageable, ExamSubjectFilterReq examSubjectFilterReq);
+
+    ResponseData<ExamSubjectEntity> create(ExamSubjectSaveReq examSubjectSaveReq);
+
+    ResponseData<ExamSubjectEntity> update(ExamSubjectSaveReq examSubjectSaveReq);
+
+    ResponseData<ExamSubjectEntity> delete(ExamSubjectDeleteReq examSubjectDeleteReq);
+}
