9b4af90c3a2212d046b4bc579bad0f01cb873184
docongban <banto2172001@gmail.com>
2025-02-08 09:21:41 +0700

    ui and integration api exam tag

---
+++ hustack-frontend/src/component/education/exam/questionbank/QuestionTagCreateUpdate.jsx
@@ -0,0 +1,144 @@
+import React, {useEffect, useState} from 'react';
+import withScreenSecurity from "../../../withScreenSecurity";
+import {
+  Box,
+  Button, Card, CardContent, CardHeader, CircularProgress,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  Input
+} from "@material-ui/core";
+import {DialogActions, MenuItem} from "@mui/material";
+import EditIcon from "@material-ui/icons/Edit";
+import TextField from "@material-ui/core/TextField";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
+import {DataGrid} from "@material-ui/data-grid";
+import useDebounceValue from "../hooks/use-debounce";
+import {request} from "../../../../api";
+import {toast} from "react-toastify";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+      width: 200,
+    },
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+    maxWidth: 300,
+  },
+}));
+function QuestionTagCreateUpdate(props) {
+
+  const classes = useStyles();
+
+  const { open, setOpen, data, isCreate} = props;
+
+  const [name, setName] = useState("")
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (data?.name) {
+      setName(data.name);
+    }
+  }, [data]);
+
+  const handleSave = () =>{
+    const body = {
+      id: data?.id,
+      name: name,
+    }
+    validateBody(body)
+
+    setIsLoading(true)
+    request(
+      "post",
+      isCreate ? `/exam-tag/create` : '/exam-tag/update',
+      (res) => {
+        if(res.status === 200){
+          if(res.data.resultCode === 200){
+            toast.success(res.data.resultMsg)
+            setIsLoading(false)
+            closeDialog()
+          }else{
+            toast.error(res.data.resultMsg)
+            setIsLoading(false)
+          }
+        }else {
+          toast.error(res)
+          setIsLoading(false)
+        }
+      },
+      { onError: (e) => toast.error(e) },
+      body
+    );
+  }
+  const validateBody = (body) => {
+    if(body.name == null || body.name === ''){
+      toast.error('Tên tag không được bỏ trống')
+      return
+    }
+  }
+
+  const closeDialog = () => {
+    setOpen(false)
+    setName("")
+  }
+
+  return (
+    <div>
+      <Dialog open={open} fullWidth maxWidth="sm">
+        <DialogTitle>{isCreate ? 'Thêm mới Tag câu hỏi' : 'Cập nhật Tag câu hỏi'}</DialogTitle>
+        <DialogContent>
+          <form className={classes.root} noValidate autoComplete="off">
+            <div>
+              <div>
+                <TextField
+                  autoFocus
+                  required
+                  id="questionTagName"
+                  label="Tên tag"
+                  placeholder="Nhập tên tag"
+                  value={name}
+                  style={{width: '100%'}}
+                  onChange={(event) => {
+                    setName(event.target.value);
+                  }}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+              </div>
+            </div>
+          </form>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            variant="contained"
+            onClick={closeDialog}
+          >
+            Huỷ
+          </Button>
+          <Button
+            disabled={isLoading}
+            variant="contained"
+            color="primary"
+            style={{marginLeft: "15px"}}
+            onClick={handleSave}
+            type="submit"
+          >
+            {isLoading ? <CircularProgress/> : "Lưu"}
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
+
+const screenName = "MENU_EXAM_QUESTION_BANK";
+// export default withScreenSecurity(QuestionBank, screenName, true);
+export default QuestionTagCreateUpdate;
---
+++ hustack-frontend/src/component/education/exam/questionbank/QuestionTagManagement.jsx
@@ -0,0 +1,171 @@
+import React, {useEffect, useState} from 'react';
+import withScreenSecurity from "../../../withScreenSecurity";
+import {
+  Box,
+  Button, Card, CardContent, CardHeader,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  Input
+} from "@material-ui/core";
+import {DialogActions, MenuItem} from "@mui/material";
+import EditIcon from "@material-ui/icons/Edit";
+import TextField from "@material-ui/core/TextField";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
+import {DataGrid} from "@material-ui/data-grid";
+import useDebounceValue from "../hooks/use-debounce";
+import {request} from "../../../../api";
+import {toast} from "react-toastify";
+import QuestionTagCreateUpdate from "./QuestionTagCreateUpdate";
+
+const baseColumn = {
+  sortable: false,
+};
+function QuestionTagManagement(props) {
+
+  const columns = [
+    {
+      field: "name",
+      headerName: "Tên",
+      flex: 1,
+      minWidth: 170,
+      ...baseColumn
+    },
+    {
+      field: "",
+      headerName: "",
+      sortable: false,
+      minWidth: 60,
+      maxWidth: 60,
+      renderCell: (rowData) => {
+        return (
+          <Box display="flex" justifyContent="space-between" alignItems='center' width="100%">
+            <EditIcon style={{cursor: 'pointer'}} onClick={(data) => handleUpdate(rowData?.row)}/>
+          </Box>
+        )
+      }
+    },
+  ];
+
+  const [keywordFilter, setKeywordFilter] = useState("")
+  const [questionTags, setQuestionTags] = useState([]);
+  const [dataRowSelected, setDataRowSelected] = useState(null);
+  const [openQuestionTagCreateUpdateDialog, setOpenQuestionTagCreateUpdateDialog] = useState(false);
+  const [isCreateQuestionTag, setIsCreateQuestionTag] = useState(true);
+
+  const debouncedKeywordFilter = useDebounceValue(keywordFilter, 500)
+
+  const { open, setOpen, data} = props;
+
+  useEffect(() => {
+    setQuestionTags(data)
+  }, [data]);
+
+  useEffect(() => {
+    console.log('keywordFilter',keywordFilter)
+  }, [debouncedKeywordFilter]);
+
+  useEffect(() => {
+    if(!openQuestionTagCreateUpdateDialog){
+      getAllQuestionTag()
+    }
+  }, [openQuestionTagCreateUpdateDialog]);
+
+  const getAllQuestionTag = () => {
+    request(
+      "get",
+      `/exam-tag/get-all`,
+      (res) => {
+        if(res.status === 200){
+          setQuestionTags(res.data)
+        }
+      },
+      { onError: (e) => toast.error(e) }
+    );
+  }
+
+  const onClickCreateNewButton = () => {
+    setOpenQuestionTagCreateUpdateDialog(true)
+    setIsCreateQuestionTag(true)
+    setDataRowSelected({
+      name: ""
+    })
+  }
+
+  const handleUpdate = (rowData) => {
+    setOpenQuestionTagCreateUpdateDialog(true)
+    setIsCreateQuestionTag(false)
+    setDataRowSelected(rowData)
+  }
+
+  const closeDialog = () => {
+    setOpen(false)
+  }
+
+  return (
+    <div>
+      <Dialog open={open} fullWidth maxWidth="sm">
+        <DialogTitle>Danh sách Tag câu hỏi</DialogTitle>
+        <DialogContent>
+          <div>
+            <Box display="flex" justifyContent="space-between" alignItems="end" width="100%" marginBottom="20px">
+              <Box display="flex" flexDirection="column" width="70%">
+                <Box display="flex" justifyContent="flex-start" width="100%">
+                  <TextField
+                    autoFocus
+                    id="examQuestionTagName"
+                    label="Nội dung tìm kiếm"
+                    placeholder="Tìm kiếm theo tên"
+                    value={keywordFilter}
+                    style={{ width: "300px", marginRight: "16px"}}
+                    onChange={(event) => {
+                      setKeywordFilter(event.target.value);
+                    }}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                  />
+                </Box>
+              </Box>
+
+              <Box display="flex" justifyContent="flex-end" width="30%">
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={onClickCreateNewButton}
+                  startIcon={<AddCircleIcon />}
+                >
+                  Thêm mới
+                </Button>
+              </Box>
+            </Box>
+            <DataGrid
+              rows={questionTags}
+              columns={columns}
+              disableColumnMenu
+              autoHeight
+            />
+          </div>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            variant="contained"
+            onClick={closeDialog}
+          >
+            Thoát
+          </Button>
+        </DialogActions>
+        <QuestionTagCreateUpdate
+          open={openQuestionTagCreateUpdateDialog}
+          setOpen={setOpenQuestionTagCreateUpdateDialog}
+          data={dataRowSelected}
+          isCreate={isCreateQuestionTag}
+        ></QuestionTagCreateUpdate>
+      </Dialog>
+    </div>
+  );
+}
+
+const screenName = "MENU_EXAM_QUESTION_BANK";
+// export default withScreenSecurity(QuestionBank, screenName, true);
+export default QuestionTagManagement;
