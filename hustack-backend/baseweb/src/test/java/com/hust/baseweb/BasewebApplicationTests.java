48018b42375c543fe09ddc90a1b725bcdeceb292
AnhTuan-AiT <anhtuan0126104@gmail.com>
2025-02-10 18:16:35 +0700

    upgrade Spring Boot to 2.7.18

--- hustack-backend/baseweb/src/test/java/com/hust/baseweb/BasewebApplicationTests.java
+++ hustack-backend/baseweb/src/test/java/com/hust/baseweb/BasewebApplicationTests.java
@@ -1,18 +1,14 @@
 package com.hust.baseweb;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
 @SpringBootTest
-@ContextConfiguration(classes = BasewebApplication.class)
-public class BasewebApplicationTests {
+class BasewebApplicationTests {
 
     @Test
-    public void contextLoads() {
+    void contextLoads() {
     }
 
 }
