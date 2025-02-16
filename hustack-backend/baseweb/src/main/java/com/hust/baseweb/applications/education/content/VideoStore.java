88d524e28b0b3f9449ed4e2ae2e728692d4d4f07
AnhTuan-AiT <58069686+AnhTuan-AiT@users.noreply.github.com>
2025-02-16 09:36:29 +0700

    Feature/upgrade spring boot 3.4.2 (#19)
    * [fix] fix API video not work after upgrading Spring Content
    
    * [chore] rm deprecated API

--- hustack-backend/baseweb/src/main/java/com/hust/baseweb/applications/education/content/VideoStore.java
+++ hustack-backend/baseweb/src/main/java/com/hust/baseweb/applications/education/content/VideoStore.java
@@ -1,10 +1,12 @@
 package com.hust.baseweb.applications.education.content;
 
-import org.springframework.content.commons.store.ContentStore;
+
+import org.springframework.content.commons.store.Store;
 import org.springframework.content.rest.StoreRestResource;
 
 import java.util.UUID;
 
 @StoreRestResource
-public interface VideoStore extends ContentStore<UUID, byte[]> {
+public interface VideoStore extends Store<UUID> {
+
 }
