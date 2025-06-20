27a207de214847143c18c757300fbc575d718643
AnhTuan-AiT <anhtuan0126104@gmail.com>
2025-06-20 22:51:46 +0700

    [fix] keep the original order

--- hustack-backend/baseweb/src/main/java/com/hust/baseweb/applications/examclassandaccount/repo/ExamClassUserloginMapRepo.java
+++ hustack-backend/baseweb/src/main/java/com/hust/baseweb/applications/examclassandaccount/repo/ExamClassUserloginMapRepo.java
@@ -2,17 +2,16 @@ package com.hust.baseweb.applications.examclassandaccount.repo;
 
 import com.hust.baseweb.applications.examclassandaccount.entity.ExamClassUserloginMap;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface ExamClassUserloginMapRepo extends JpaRepository<ExamClassUserloginMap, UUID> {
 
-    @Query("SELECT e FROM ExamClassUserloginMap e " +
-           "WHERE e.examClassId = :examClassId " +
-           "ORDER BY CASE WHEN e.randomUserLoginId IS NULL THEN 0 ELSE 1 END ASC, " +
-           "e.realUserLoginId ASC")
+//    @Query("SELECT e FROM ExamClassUserloginMap e " +
+//           "WHERE e.examClassId = :examClassId " +
+//           "ORDER BY CASE WHEN e.randomUserLoginId IS NULL THEN 0 ELSE 1 END ASC, " +
+//           "e.realUserLoginId ASC")
     List<ExamClassUserloginMap> findByExamClassId(UUID examClassId);
 
     List<ExamClassUserloginMap> findByExamClassIdAndStatus(UUID examClassId, String status);
